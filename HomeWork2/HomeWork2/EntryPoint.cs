91691d1acd65181f0b03059668f4ac5360d0e73b
Kirill Novik <thenavik1337@gmail.com>
2021-04-15 22:17:11 +0300

    Add method GetMaxCCountOfUniqueSequenceOfLetters and rename Program to EntryPoint

---
+++ HomeWork2/HomeWork2/EntryPoint.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMaxCountOfUniqueSequenceOfLetters(args[0]));     
+        }
+
+
+        static int GetMaxCountOfUniqueSequenceOfLetters(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int result = 1;
+            int count = 1;
+            for(int i = 0; i < str.Length-1; i++)
+            {
+                if (str[i] != str[i + 1])
+                {
+                    count++;             
+                }
+
+                if (str[i] == str[i + 1])
+                {
+                    if(count > result)
+                    {
+                        result = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (count > result)
+            {
+                result = count;
+            }
+
+            return result;
+        }
+    }
+}
