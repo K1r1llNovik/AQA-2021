61f0650eabfbe3292064a66faee5f73398438ea9
Kirill Novik <thenavik1337@gmail.com>
2021-04-26 13:19:53 +0300

    Add class Car

---
+++ HomeWork3/HomeWork3/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork3
+{
+    public class Car : Vehicle
+    {
+        public ushort CarryingCapacity { get; }
+
+        public Car(double engineVolume, string transmissionType, ushort maxSpeed, ushort carryingCapacity) : base(engineVolume, transmissionType, maxSpeed)
+        {
+            CarryingCapacity = carryingCapacity;
+        }
+
+        public override void GetFullInfo()
+        {
+            base.GetFullInfo();
+            Console.WriteLine("CarryingCapacity: " + CarryingCapacity);
+        }
+
+
+
+
+    }
+}
--- HomeWork3/HomeWork3/Vehicle.cs
+++ HomeWork3/HomeWork3/Vehicle.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace HomeWork3
 {
-    class Vehicle
+    abstract class Vehicle
     {
+
+        private double EngineVolume { get; }
+        private string TransmissionType { get; }
+        private ushort MaxSpeed { get; }
+
+        public Vehicle(double engineVolume, string transmissionType, ushort maxSpeed)
+        {
+            EngineVolume = engineVolume;
+            TransmissionType = transmissionType;
+            MaxSpeed = maxSpeed;
+        }
+
+
+        public virtual void GetFullInfo()
+        {
+            Console.WriteLine("/nEngine Volume: " + EngineVolume + "/nTransmissionType: " + TransmissionType + "/nMax speed: " + MaxSpeed);
+        }
+
     }
 }
